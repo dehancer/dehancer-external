b87ce51529a59ed13c68e584a5d0c511b3d75dab
avlkharkov <avl.kharkov@gmail.com>
2024-11-14 20:25:41 +0100

    Node type experiment

--- OpenFXS/Library/ofxsImageEffect.cpp
+++ OpenFXS/Library/ofxsImageEffect.cpp
@@ -1876,7 +1876,7 @@ namespace OFX {
             std::string nodetype = hostProps.propGetString("uk.ltd.filmlight.nodetype", false);
 
             gHostDescription.hostName                   = hostProps.propGetString(kOfxPropName, true);
-            OFX::Log::print("Host name %s, Node type %s", gHostDescription.hostName, nodetype);
+            OFX::Log::print("Host name %s, Node type %s", gHostDescription.hostName.c_str(), nodetype.c_str());
             gHostDescription.hostLabel                  = hostProps.propGetString(kOfxPropLabel, true);
             gHostDescription.versionMajor               = hostProps.propGetInt(kOfxPropVersion, 0, false); // OFX 1.2
             gHostDescription.versionMinor               = hostProps.propGetInt(kOfxPropVersion, 1, false); // OFX 1.2
