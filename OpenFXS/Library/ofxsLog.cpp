7605f343b73997b37f4a1830033ba9da0e4f94bc
denn nevera mb pro m2 <denn.nevera@gmail.com>
2024-11-29 18:28:26 +0000

    log

--- OpenFXS/Library/ofxsLog.cpp
+++ OpenFXS/Library/ofxsLog.cpp
@@ -51,7 +51,6 @@ The log file is written to using printf style functions, rather than via c++ ios
 
 namespace OFX {
   namespace Log {
-
     /** @brief log file */
     static FILE *gLogFP = 0;
 
@@ -59,20 +58,26 @@ namespace OFX {
 #define kLogFileEnvVar "OFX_PLUGIN_LOGFILE"
 
     /** @brief the global logfile name */
-    static std::string gLogFileName(getenv(kLogFileEnvVar) ? getenv(kLogFileEnvVar) : "ofxTestLog.txt");
+    // static std::string gLogFileName(getenv(kLogFileEnvVar) ? getenv(kLogFileEnvVar) : "ofxTestLog.txt");
+
+    static std::string gLogFileName(
+      std::getenv(kLogFileEnvVar)
+        ? std::getenv(kLogFileEnvVar)
+#if WIN32
+        : std::getenv("TEMP") ? std::string(std::getenv("TEMP")) + "\\ofxTestLog.txt"
+#endif
+        : "/tmp/ofxTestLog.txt");
 
     /** @brief global indent level, not MP sane */
     static int gIndent = 0;
 
     /** @brief Sets the name of the log file. */
-    void setFileName(const std::string &value)
-    {
+    void setFileName(const std::string &value) {
       gLogFileName = value;
     }
 
     /** @brief Opens the log file, returns whether this was sucessful or not. */
-    bool open(void)
-    {
+    bool open(void) {
 #ifdef DEBUG
       if(!gLogFP) {
         gLogFP = fopen(gLogFileName.c_str(), "w");
@@ -83,40 +88,35 @@ namespace OFX {
     }
 
     /** @brief Closes the log file. */
-    void close(void)
-    {
-      if(gLogFP) {
+    void close(void) {
+      if (gLogFP) {
         fclose(gLogFP);
       }
       gLogFP = 0;
     }
 
     /** @brief Indent it, not MP sane at the moment */
-    void indent(void)
-    {
+    void indent(void) {
       ++gIndent;
     }
 
     /** @brief Outdent it, not MP sane at the moment */
-    void outdent(void)
-    {
+    void outdent(void) {
       --gIndent;
     }
 
     /** @brief do the indenting */
-    static void doIndent(void)
-    {
-      if(open()) {
-        for(int i = 0; i < gIndent; i++) {
+    static void doIndent(void) {
+      if (open()) {
+        for (int i = 0; i < gIndent; i++) {
           fputs("    ", gLogFP);
         }
       }
     }
 
     /** @brief Prints to the log file. */
-    void print(const char *format, ...)
-    {
-      if(open()) {
+    void print(const char *format, ...) {
+      if (open()) {
         doIndent();
         va_list args;
         va_start(args, format);
@@ -128,9 +128,8 @@ namespace OFX {
     }
 
     /** @brief Prints to the log file only if the condition is true and prepends a warning notice. */
-    void warning(bool condition, const char *format, ...)
-    {
-      if(condition && open()) {
+    void warning(bool condition, const char *format, ...) {
+      if (condition && open()) {
         doIndent();
         fputs("WARNING : ", gLogFP);
 
@@ -145,9 +144,8 @@ namespace OFX {
     }
 
     /** @brief Prints to the log file only if the condition is true and prepends an error notice. */
-    void error(bool condition, const char *format, ...)
-    {
-      if(condition && open()) {
+    void error(bool condition, const char *format, ...) {
+      if (condition && open()) {
         doIndent();
         fputs("ERROR : ", gLogFP);
 
